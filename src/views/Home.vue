2355d68bcd255caf0331252763a98b99601be690
Andrei <andrei.hudalla@outlook.com>
2023-04-05 18:45:46 +0300

    Added team

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 </script>
 
 <template>
@@ -10,9 +11,56 @@
       </router-link>
     </div>
   </div>
+  <div class="team">
+    <h1>Team @ Monsta Games</h1>
+    <h2>Consists of:</h2>
+    <ul>
+      <li>
+        Founder (Builder, Voice Actor)
+      </li>
+      <li>
+        Director (Storywriter, UI Designer, Game Director)
+      </li>
+      <li>
+        4 Proficient programmers
+      </li>
+      <li>
+        Media managers
+      </li>
+      <li>
+        Highly entity modeler
+      </li>
+    </ul>
+    <p>This is how Monsta Games continues to produce high quality projects</p>
+  </div>
 </template>
 
 <style scoped>
+.team {
+  backdrop-filter: blur(0.5rem);
+  display: flex;
+  flex-direction: column;
+  border-top: rgba(255, 255, 255, 0.3) 0.2vh dashed;
+  background-color: rgba(255, 255, 255, 0.08);
+  padding: 2rem 30vw;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  color: white;
+  margin: 0.5rem;
+}
+
+li {
+  text-align: left;
+  color: white;
+}
+
 #logo {
   width: 50vh;
   transition: all 1s;
@@ -33,7 +81,14 @@ main {
   padding: 1rem;
 }
 
-main h1, main h2, main h3, main h4, main h5, main h6, main p, main span {
+main h1,
+main h2,
+main h3,
+main h4,
+main h5,
+main h6,
+main p,
+main span {
   color: white;
 }
 
@@ -63,8 +118,12 @@ section {
   button {
     margin-top: 10vh;
   }
+
   #logo {
     width: 95vw;
+  }
+  .team {
+    padding: 2rem 0;
   }
 }
 </style>
